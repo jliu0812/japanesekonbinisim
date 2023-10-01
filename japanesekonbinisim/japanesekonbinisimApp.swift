933d43b19e72a780055bdf192c589be2eba3628a
jliu0812 <jeremyliujunhong@gmail.com>
2023-10-01 12:05:41 -0500

    Initial Commit

---
+++ japanesekonbinisim/japanesekonbinisimApp.swift
@@ -0,0 +1,17 @@
+//
+//  japanesekonbinisimApp.swift
+//  japanesekonbinisim
+//
+//  Created by Jeremy Liu on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct japanesekonbinisimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
